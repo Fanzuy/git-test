c3b56f6692b5c4bb84ac9ecb4de0390a1f5ef5d1
Fanzy <1260061522@qq.com>
2020-04-10 21:16:00 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+/**
+ * @author fzy
+ * @date 20-4-10 - 21:04
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
