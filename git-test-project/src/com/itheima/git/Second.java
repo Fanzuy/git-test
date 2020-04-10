24a52e45d9625f01b1f8a0079d378a1265d24152
Fanzy <1260061522@qq.com>
2020-04-10 21:31:58 +0800

    添加一个second类

---
+++ git-test-project/src/com/itheima/git/Second.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+/**
+ * @author fzy
+ * @date 20-4-10 - 21:30
+ */
+public class Second {
+    public void test() {
+        System.out.println("hello second");
+    }
+}
